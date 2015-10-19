8125e826fae9ca66243fd82882e8a94950253e91
namson168 <namsonqd@gmail.com>
2015-10-19 13:52:55 +0700

    temp

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ENV.update YAWL.load_file('config/application.yml')[Rails.env] rescue {}
+
+
   end
 end
